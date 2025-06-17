6cf39a26d21c3bebbc0216a75b970b6ae98c37d1
Swastik Nandy <nswastik2003@gmail.com>
2025-06-17 16:58:20 +0530

    modified start.sh + conf

--- backend/start.sh
+++ backend/start.sh
@@ -7,5 +7,5 @@ python manage.py collectstatic --noinput
 echo "▶ Running migrations"
 python manage.py migrate --noinput
 
-echo "▶ Starting Gunicorn on port ${PORT:-8000}"
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+echo "▶ Starting Gunicorn on port $PORT"
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
